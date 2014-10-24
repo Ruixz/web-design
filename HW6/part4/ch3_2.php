b8eee90aab4c07a8cfdbd6f48de9b12e748ffc32
Ruixz <raysong0309@hotmail.com>
2014-10-23 21:18:24 -0400

    add HW6

---
+++ HW6/part4/ch3_2.php
@@ -0,0 +1,8 @@
+<?php
+$x=3;
+$y=4;
+echo "Test 1 result: " . ($x==$y) . " <br /> ";
+echo "Test 2 result: " . ($x>$y) . " <br /> ";
+echo "Test 3 result: " . ($x<=$y) . " <br /> ";
+echo "Test 4 result: " . ($x!=$y) . " <br /> ";
+?>
